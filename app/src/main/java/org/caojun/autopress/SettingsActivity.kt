80051957d471dfe6d8327b98ca7f5b292233419b
fourmigh <fourmi_103683@hotmail.com>
2022-04-10 18:17:29 +0800

    完成基本功能

--- app/src/main/java/org/caojun/autopress/SettingsActivity.kt
+++ app/src/main/java/org/caojun/autopress/SettingsActivity.kt
@@ -1,8 +1,11 @@
 package org.caojun.autopress
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import org.caojun.library.accessibilityservice.MainAccessibilityService
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -21,6 +24,15 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (MainAccessibilityService.isDestroyed()) {
+            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
         }
     }
 }
